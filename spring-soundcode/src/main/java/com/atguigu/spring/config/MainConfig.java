06ee337b4f028991aa100cb964bc526eab3e4ecb
CMY <137265690@qq.com>
2022-11-08 09:21:49 +0800

    Spring SoundCode testModule

--- spring-soundcode/src/main/java/com/atguigu/spring/config/MainConfig.java
+++ spring-soundcode/src/main/java/com/atguigu/spring/config/MainConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan.Filter;
 
 //@Import({Person.class, MyBeanDefinitionRegistry.class})
 @ComponentScan(value = {"com.atguigu.spring.bean","com.atguigu.spring.processor","com.atguigu.spring.circle"},useDefaultFilters = true , includeFilters = {@Filter(type = FilterType.ASSIGNABLE_TYPE ,classes = {Teacher.class, Cat.class})})
-@Configuration
+@Configuration()
 public class MainConfig {
 
 //	@Bean
